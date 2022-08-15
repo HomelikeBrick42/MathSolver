a1eb25665a00656d48bb34a4ade9e726f4d0addf
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2022-08-16 11:29:22 +1200

    Added repl

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,38 +1,31 @@
+use std::io::Write;
+
 use math::*;
-use num_rational::BigRational;
 
 fn main() {
-    let equation = Equation::Equality {
-        left: Expression {
-            terms: vec![Term {
-                atoms: vec![Atom::Fraction {
-                    numerator: Expression {
-                        terms: vec![
-                            Term {
-                                atoms: vec![
-                                    Atom::Number(BigRational::from_float(909.0).unwrap()),
-                                    Atom::Variable("y".into()),
-                                ],
-                            },
-                            Term {
-                                atoms: vec![Atom::Number(BigRational::from_float(5.0).unwrap())],
-                            },
-                        ],
-                    },
-                    denominator: Expression {
-                        terms: vec![Term {
-                            atoms: vec![Atom::Number(BigRational::from_float(116.0).unwrap())],
-                        }],
-                    },
-                }],
-            }],
-        },
-        right: Expression {
-            terms: vec![Term {
-                atoms: vec![Atom::Number(BigRational::from_float(1246.0).unwrap())],
-            }],
-        },
-    }; // should be 159
-    println!("{}", equation);
-    println!("{}", simplify(&equation));
+    'main_loop: loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        let line = std::io::stdin()
+            .lines()
+            .next()
+            .unwrap()
+            .expect("read a line from the console");
+        if line.len() > 0 && line.chars().nth(0).unwrap() == ':' {
+            match &line as &str {
+                ":exit" => break 'main_loop,
+                _ => println!("Unknown command '{line}'"),
+            }
+        }
+        let mut lexer = Lexer::new("stdin", &line);
+        match parse_equation(&mut lexer) {
+            Ok(equation) => {
+                println!("{}", equation);
+                println!("{}", simplify(&equation));
+            }
+            Err(error) => {
+                println!("{}", error);
+            }
+        }
+    }
 }
